1750fc21cacd0cb290902f5e8fdc074d081f6e23
it <princehsylvanus@gmail.com>
2024-02-20 16:28:14 +0100

    nav2

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -16,16 +16,26 @@ const Sidenav = () => {
         <div className={Style.firstBox}>
 
           <div className={Style.iconMenu}>
-            <IoMdHome size={24} />
-            <p>Home</p>
+
+            <div className={Style.iconMenuContent}>
+
+              <IoMdHome size={24} />
+              <p>Home</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Short</p>
+
+            <div className={Style.iconMenuContent}>
+
+              <SiShortcut />
+              <p>Short</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>Subscription</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Subscription</p>
+            </div>
           </div>
 
         </div>
@@ -34,28 +44,44 @@ const Sidenav = () => {
         <div className={Style.firstBox}>
 
           <div className={Style.iconMenu}>
-            <p>You</p>
-            < GoChevronRight ize={24} />
+
+            <div className={Style.iconMenuContent}>
+
+              <div className={Style.iconMenuContent}>
+                <p>You</p>
+                < GoChevronRight ize={24} />
+              </div>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Your Channel</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Your Channel</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>History</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>History</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Play List</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Play List</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>Watched later</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Watched later</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>Liked videos</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Liked videos</p>
+            </div>
           </div>
 
         </div>
@@ -65,61 +91,164 @@ const Sidenav = () => {
         <div className={Style.firstBox}>
 
           <div className={Style.iconMenu}>
-            <p>SUBCRIPTIONS</p>
-            {/* < GoChevronRight ize={24} /> */}
+            <div className={Style.iconMenuContent}>
+              <p>SUBCRIPTIONS</p>
+              {/* < GoChevronRight ize={24} /> */}
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Your Channel</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Your Channel</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>History</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>History</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Play List</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Play List</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>Watched later</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Watched later</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>Liked videos</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Liked videos</p>
+            </div>
           </div>
 
         </div>
 
         {/* explore */}
         <hr />
-   
+
         <div className={Style.firstBox}>
 
           <div className={Style.iconMenu}>
-            <p>EXPLORE</p>
-            {/* < GoChevronRight ize={24} /> */}
+            <div className={Style.iconMenuContent}>
+              <p>EXPLORE</p>
+              {/* < GoChevronRight ize={24} /> */}
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Trending</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Trending</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Music</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Music</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <SiShortcut />
-            <p>Gaming</p>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Gaming</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>News</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>News</p>
+            </div>
           </div>
           <div className={Style.iconMenu}>
-            <MdOutlineSubscriptions />
-            <p>Sports</p>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Sports</p>
+            </div>
           </div>
+
+        </div>
+
+        {/* MORE FROM YOUTUBE */}
+        <hr />
+
+        <div className={Style.firstBox}>
+
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <p>MORE FROM YOUTUBE</p>
+              {/* < GoChevronRight ize={24} /> */}
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Trending</p>
+            </div>
+
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Music</p>
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Gaming</p>
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>News</p>
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <MdOutlineSubscriptions />
+              <p>Sports</p>
+            </div>
+          </div>
+
+        </div>
+
+        {/* SETTINGS */}
+        <hr />
+
+        <div className={Style.firstBox}>
+
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <p>SETTINGS</p>
+              {/* < GoChevronRight ize={24} /> */}
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Report History</p>
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Help</p>
+            </div>
+          </div>
+          <div className={Style.iconMenu}>
+            <div className={Style.iconMenuContent}>
+              <SiShortcut />
+              <p>Send feedback</p>
+            </div>
+          </div>
+          
+          
 
         </div>
 
