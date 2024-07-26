13d81de3677ec62c5ed9cf5f71fe34f1f997477b
Sylvanus Prince <princehsylvanus@gmail.com>
2024-07-26 11:22:09 +0100

    removed comments not needed

--- src/components/navbars/videos/all-video.jsx
+++ src/components/navbars/videos/all-video.jsx
@@ -1,31 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-// import InfiniteScroll from "react-infinite-scroll-component";
-// import ReactPaginate from "react-paginate";
 import Style from "../../../CSS styles/allvideos.module.css";
-// import styled from "../../../CSS styles/paginate.module.css";
-// import testImg from "../../../images/testImg.mp4";
-// import ReactPlayer from "react-player";
-// import TestImage from "../../../images/test.jpg";
 import VideoCard from "./video-card";
-// import TestComp from "./testComp";
-
 const AllVideos = () => {
   const [movieData, setMovieData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   const apiKey = "8a75e9def8895d8c1f9e824dc7033473"; 
   const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
-// const increment = () => {
-//     setPage((prevCount) => (prevCount >= 500 ? 1 : prevCount + 1));
-//   };
-
-//   const decrement = () => {
-//     setPage((prevCount) => (prevCount <= 1 ? 500 : prevCount - 1));
-//   };
-
   useEffect(() => {
     const fetchMovieData = async (page) => {
       try {
@@ -64,14 +47,10 @@ const AllVideos = () => {
     };
   }, [loading]);
 
-
   return (
     <div className={Style.Container}>
-      
-        
           <div className={Style.movieDataBox}>
             {movieData.map((item) => {
-              
               return (
                 <VideoCard
                   // key={index}
@@ -90,22 +69,7 @@ const AllVideos = () => {
               {loading && <p className="loading">Loading...</p>}
             </div>
           </div>
-        
-      
-
-      
-
-      {/* <div className={styled.paginated}>
-        <button onClick={decrement} className={styled.nextBtn}>
-          Prev
-        </button>
-
-        <button onClick={increment} className={styled.prevBtn}>
-          Next
-        </button>
-      </div> */}
     </div>
   );
 };
-
 export default AllVideos;
