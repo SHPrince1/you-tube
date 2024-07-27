995b9686aa8b327851a25d335ff17db04a4cb9f1
Sylvanus Prince <princehsylvanus@gmail.com>
2024-07-27 07:35:30 +0100

    Over view effect removed

---
+++ src/components/input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CustomInput = ({ query, setQuery }) => {
+  return (
+    <div>
+
+        <input 
+        
+        />
+    </div>
+  )
+}
+
+export default CustomInput
